29909421d4e8e99fa0246c2d2c571ec88b415020
Daniiel356 <255756069+Daniiel356@users.noreply.github.com>
2026-09-06 17:10:07 -0600

    update scripts/connection/conection.js

--- scripts/connection/conection.js
+++ scripts/connection/conection.js
@@ -1,37 +1,50 @@
 import { Conn } from "./wsConnection.js";
 
 export class Connection{
+    #context;
     #conn=new Conn();
-    #initResolve=()=>{};
-    #initReject=()=>{};
     _host=false;
     _state=-1;
     _id=-1;
     on=()=>{};
 
-    constructor(){
+    constructor(context){
+        this.#context=context;
         fetch("https://testserver-h5lx.onrender.com").catch((e)=>{
             console.log("ERROR al conectar")
         });
-        this.#conn.out=(e)=>this.#onMsg(e);
     }
 
     async connectToServer(){
         console.log("Conectando al server...");
-        try{
-            await this.#conn.init();
-            console.log("Conectado con exito!, esperando primer mensaje..");
-            this.#update();
-            return new Promise((res, rej)=>{
-                this.#initResolve=res;
-                this.#initReject=rej;
-            });
-        }catch(err){
-            this.#initReject(err);
-        }
+        return new Promise((res, rej)=>{
+            try{
+                await this.#conn.init();
+                console.log("Conectado con exito!, esperando primer mensaje..");
+                const data=await new Promise((res, rej)=>{
+                    setTimeout(()=>this._id!=-1&&rej("Id Timeout exception"), 5000);
+                    #this.#conn.out=(e)=>{
+                        const msg=JSON.parse(msg);
+                        if(msg.first){
+                            res([msg.id, msg.host]);
+                        }
+                    }
+                });
+                this._id=data[0];
+                this._host=data[1];
+                this.#conn.out=(e)=>this.#onMsg(e);
+                this.#update();
+                res(data);
+            }catch(err){
+                rej(err);
+            }
+        });
     }
 
-    request(){}
+    postInput(id, input){
+        console.log("redireccionando input")
+        this.#context.setInput(id, input);
+    }
 
     #update(){
         this._state=this.#conn.state;
@@ -39,11 +52,5 @@ export class Connection{
 
     #onMsg(e){
         const msg=JSON.parse(e.data);
-        if(msg.first){
-            this._host=msg?.host||false;
-            this._id=msg?.id||-1;
-            this.#initResolve();
-            console.log("host:",msg.host," id:",msg.id);
-        }
     }
 }
