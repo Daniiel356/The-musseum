73a4950af90a31f3491cfd137ca9957be1929526
Daniiel356 <255756069+Daniiel356@users.noreply.github.com>
2026-09-07 16:33:21 -0600

    update scripts/connection/wsConnection.js

--- scripts/connection/wsConnection.js
+++ scripts/connection/wsConnection.js
@@ -1,37 +1,51 @@
-export class Conn {
+export class Conn{
     #ws;
     out=()=>{};
-    get state(){return this.#ws?.readyState || -1};
+
+    get state(){
+        return this.#ws?.readyState || -1;
+    }
 
     async init(){
-        return new Promise((res, rej)=>{
-            let intents=10;
-            let error;
+        let intents=10;
+        let error;
 
-            while(intents>0){
-                this.#ws=new WebSocket("wss://testserver-h5lx.onrender.com");
-        
-                this.#ws.onopen=()=>{
-                    break;
-                };
-    
-                this.#ws.onerror=(err)=>{
-                    intents--;
-                    error=err;
-                    continue;
-                };
+        while(intents>0){
+            try{
+                await new Promise((res, rej)=>{
+                    this.#ws=new WebSocket(
+                        "wss://testserver-h5lx.onrender.com"
+                    );
+
+                    this.#ws.onopen=()=>{
+                        res();
+                    };
+
+                    this.#ws.onerror=(err)=>{
+                        rej(err);
+                    };
+                });
+
+                // La conexión funcionó
+                break;
+
+            }catch(err){
+                intents--;
+                error=err;
+
+                console.log(`Intento fallido. Quedan ${intents} intentos`);
             }
-            if(intents==0){
-                rej(error);
-            }
-            
-            this.#ws.onmessage=(msg)=>{
-                this.out(msg);
-            };
-            res();
-        });
+        }
+
+        if(intents==0){
+            throw error;
+        }
+
+        this.#ws.onmessage=(msg)=>{
+            this.out(msg);
+        };
     }
-    
+
     send(msg){
         this.#ws.send(msg);
     }
